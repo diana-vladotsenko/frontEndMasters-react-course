50fe8ce6f8849bb7c70e1825d4269e9b7b2d7942
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-02-05 17:21:18 +0200

    added prettier, eslist ja made some configuration

--- src/app.js
+++ src/app.js
@@ -1,31 +1,33 @@
-const Pizza=(props)=>{
-    return React.createElement(
-        "div", null, 
-        React.createElement("p",{},[
-            React.createElement(props.name),
-            React.createElement(props.description)
-        ])
-    )
-}
+const Pizza = (props) => {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement("p", {}, [
+      React.createElement(props.name),
+      React.createElement(props.description),
+    ]),
+  );
+};
 
-const App = ()=>{
-    return React.createElement("div", null, [
-        React.createElement("h1",{},"Pizza Padre Gino"),
-        React.createElement(Pizza,{
-            name: "Pepperoni",
-            description: "Pepperoni, cheese, tomato sauce"
-        }),
-        React.createElement(Pizza,{
-            name: "Margherita",
-            description: "Cheese, tomato sauce, basil"
-        }),
-        React.createElement(Pizza,{
-            name: "Quattro Stagioni",
-            description: "Pepperoni, ham, artichokes, mushrooms"
-        })
-    ]
-)}
+const App = () => {
+    debugger;
+  return React.createElement("div", null, [
+    React.createElement("h1", {}, "Pizza Padre Gino"),
+    React.createElement(Pizza, {
+      name: "Pepperoni",
+      description: "Pepperoni, cheese, tomato sauce",
+    }),
+    React.createElement(Pizza, {
+      name: "Margherita",
+      description: "Cheese, tomato sauce, basil",
+    }),
+    React.createElement(Pizza, {
+      name: "Quattro Stagioni",
+      description: "Pepperoni, ham, artichokes, mushrooms",
+    }),
+  ]);
+};
 
-const container= document.getElementById("root");
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App)); 
+root.render(React.createElement(App));
